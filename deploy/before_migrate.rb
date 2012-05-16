95960c7539125aeb9f79953cd5fa01cd30355484
David Joos <david.joos@gmail.com>
2012-05-16 16:21:28 +0000

    initial commit

---
+++ deploy/before_migrate.rb
@@ -0,0 +1,8 @@
+log " *** BEFORE MIGRATE *** "
+
+#in order for migrations to run your entire environment is loaded
+#so: if you have any symlinks that need to be created in order for the application to start properly,
+#put them in before_migrate.rb instead of before_symlink.rb, because before_symlink.rb runs after the migration!
+
+log "#{node.chef_environment}"
+log "#{@new_resource.environment_name}"
---
+++ deploy/before_symlink.rb
@@ -0,0 +1,1 @@
+log " *** BEFORE SYMLINK *** "
