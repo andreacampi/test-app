e766263da16cfab58d2098ce82a1eb19d0e950fa
Andrea Campi <andrea.campi@zephirworks.com>
2012-05-24 23:40:36 +0200

    New feature: new_resource responds to #release_path and shared_path.

--- deploy/before_migrate.rb
+++ deploy/before_migrate.rb
@@ -6,6 +6,5 @@ log " *** BEFORE MIGRATE *** "
 
 log "#{node.chef_environment}"
 log "#{@new_resource.environment_name}"
-
-log "#{@new_resource.inspect}"
-log "#{@deploy_resource.inspect}"
+log "#{@new_resource.shared_path}"
+log "#{@new_resource.release_path}"
